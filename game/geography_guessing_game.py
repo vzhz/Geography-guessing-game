4d8b598d470fefae6ae73543798de1fb77d96e24
vzhz <vehanus@gmail.com>
2015-10-28 22:00:33 -0400

    ask order, ask repeat use enum

--- game/geography_guessing_game.py
+++ game/geography_guessing_game.py
@@ -1,63 +1,61 @@
 import random
 import unittest
 import sys
+from gamestate import Game, Mode
 
-def fancy_print(string, i=1): #does this automatically come after each string if //
-							#you call it at the end?
+def fancy_print(string, i=1): 						
     # Prints out a string with i newlines after it
     print string
     print "\n"*i #use fancy_print instead of print
 
-def choose_order_in_case_of_placeholder():
-	mode = random.choice([1, 2])
-	return mode
+def question_to_answer_dict():
+	state_capital_pairs = Set()
+	with open('state_capitals.txt', 'r').read().splitlines() as f:
+		for line in f:
+			state_capital_pairs.add = line.split(',')
 
-def question_to_answer_dict(mode):
-	state_capital = {}
-	capital_state = {}
-	f = open('state_capitals.txt', 'r').read().splitlines()
-	for line in f:
-		state, capital = line.split(',')
-		state_capital[state] = capital
-		capital_state[capital] = state
-
-	if mode == 1:
-		q_a_dict = state_capital #but this is an empty dict
-	if mode == 2:
-		q_a_dict = capital_state
-	return q_a_dict
-
-def compute_mode():
-	#print sys.argv gives ['scrabble.py', '<whatever I typed after command in terminal>']
-	mode_str = sys.argv[1].lower()
+# def compute_mode():
+# 	#print sys.argv gives ['scrabble.py', '<whatever I typed after command in terminal>']
+# 	mode_str = sys.argv[1].lower()
+# 	while True:
+# 		if mode_str == "guess capital" or mode_str == "capital":
+# 			mode = 1
+# 		elif mode_str == "guess state" or mode_str == "state":
+# 			mode = 2
+# 		elif mode_str == "mix it up!" or mode_str == "mix it up" or mode_str == "mix": 
+# 			mode = random.choice([1, 2]
+# 		#ask zulip folks if they know how to make !!! not matter, accept more variations
+# 			#mode(state_capital, mode) #how to make random back and forth maybe start with random number and 
+# 		#Should the game tell you "This is a state, give me the capital" in that case? Or should the player have to figure that out themself too?
+# 		#figure out what should replace "state_capital" in line 19
+# 		else:
+# 			mode_str = raw_input("Please type or 'guess capital', 'guess state', or 'mix it up!' after name of program to start")
+# 			continue
+# 		break	
+# 	return mode
+def ask_mode():
 	while True:
-		if mode_str == "guess capital" or mode_str == "capital":
-			mode = 1
-		elif mode_str == "guess state" or mode_str == "state":
-			mode = 2
-		elif mode_str == "mix it up!" or mode_str == "mix it up" or mode_str == "mix": 
-			mode = choose_order_in_case_of_placeholder()
-		#ask zulip folks if they know how to make !!! not matter, accept more variations
-			#mode(state_capital, mode) #how to make random back and forth maybe start with random number and 
-		#Should the game tell you "This is a state, give me the capital" in that case? Or should the player have to figure that out themself too?
-		#figure out what should replace "state_capital" in line 19
-		else:
-			mode_str = raw_input("Please type or 'guess capital', 'guess state', or 'mix it up!' after name of program to start")
-			continue
-		break	
-	return mode
+		user_input = (raw_input("Type 'guess capital' if you want to guess capitals given states, \n \
+			'guess state' if you want to guess states given capitals, \n \
+			and 'random' if you want a mix of both!")).lower()
+		if user_input == "guess capital":
+			return Mode.capital
+		if user_input == "guess state":
+			return Mode.state
+		if user_input == "random":
+			return Mode.random
+		print "Learn to type, punk."
 
-def compute_repeat_questions():
-	repeat_questions = sys.argv[2].lower()
-	if repeat_questions == "repeat":
-		repeat_questions = "repeat"
-		#repeat_questions(, repeat_questions) #<--what there
-	elif repeat_questions == "cover it all":
-		repeat_questions = "cover it all"
-		#repeat_questions(, repeat_questions) #<--what there
-	else:
-		print "Please type or 'repeat' or 'cover it all' after question type to start"
-	return repeat_questions
+
+def ask_repeat_questions():
+	while True:
+		user_input = (raw_input("Type 'repeat' and enter if you want to see state/capital pairs more than once \
+			and just enter if you do not want pairs to repeat.")).lower()
+		if user_input == "repeat":
+			return True
+		if user_input == "":
+			return False
+		print "Invalid input, you bumble."
 
 def asks_user_question(mode, key):
 	if mode == 1:  
@@ -87,6 +85,8 @@ def game(q_a_dict, mode, repeat_questions):
 	points = 0
 	count = 1.0 #float
 	keys = q_a_dict.keys()
+	mode = int(raw_input("")) # should I call this "order"?
+	repeat = int(raw_input("")) # still best as boolean?
 	turns = int(raw_input("How many rounds would you like to play? (We'll remind you when you're getting close!)")) 
 	fancy_print("%d is a great number of rounds! Go you!" % turns)
 	while True:
